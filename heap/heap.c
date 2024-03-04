dec9d6cfd4a65d196c18498d02eaaee95142ca7f
Arevalinchy <75390468+Arevalinchy@users.noreply.github.com>
2024-03-04 16:58:46 +0100

    Update heap.c

--- heap/heap.c
+++ heap/heap.c
@@ -3,15 +3,22 @@
 
 heap heap_create(int k, int (*f)(const void *, const void *)) {
   if(k!=0){
-    heap bestoTSA = (heap*) malloc(sizeof(heap));
-    bestoTSA.f = f
+    heap bestoTSA = malloc(sizeof(bestoTSA));
+    bestoTSA
+    bestoTSA->n = 0;
+    bestoTSA->nmax = 10;
+    bestoTSA->f = f;
   }
-
+  
   return NULL;
 }
 
 void heap_destroy(heap h) {
-  
+  while(!heap_empty(h)){
+    heap_top(h);
+  }
+  free(h-> array);
+  free(h); 
 }
 
 bool heap_empty(heap h) {
@@ -22,9 +29,7 @@ bool heap_empty(heap h) {
 }
 
 bool heap_add(heap h, void *object) {
-  ;
-  ;
-  ;
+ //insert le 
   return false;
 }
 
