8a141d568e6075d6e2f4d68bca3929e0f714a13f
Arevalinchy <75390468+Arevalinchy@users.noreply.github.com>
2024-03-06 15:32:10 +0100

    Update heap.c

--- heap/heap.c
+++ heap/heap.c
@@ -3,46 +3,52 @@
 
 heap heap_create(int k, int (*f)(const void *, const void *)) {
   if(k!=0){
-    heap bestoTSA = malloc(sizeof(bestoTSA));
-    bestoTSA
+    heap bestoTSA = malloc(sizeof(*bestoTSA));
     bestoTSA->n = 0;
-    bestoTSA->nmax = 10;
+    bestoTSA->nmax = k;
+    bestoTSA->array = malloc((k+1)*sizeof(void*));
     bestoTSA->f = f;
+    return bestoTSA;
   }
-  
   return NULL;
 }
 
 void heap_destroy(heap h) {
   while(!heap_empty(h)){
-    heap_top(h);
+    heap_pop(h);
   }
   free(h-> array);
   free(h); 
 }
 
 bool heap_empty(heap h) {
-  ;
-  ;
-  ;
+  if(h->n != 0) return false;
   return true;
 }
 
-bool heap_add(heap h, void *object) {
- //insert le 
+//manque cette fonction
+bool heap_add(heap h, void *object) {   
+  if(h->n == h->nmax) return true;
+  h->array[++(h->n)] = object;
+  void* temp; 
+  while(h->f(h->array[n],h->array[n/2]) < 0){
+    temp = h->array[n/2];
+    h->array[n/2] = h->array[n];
+    h->array[n] = temp;
+  }
   return false;
 }
 
 void *heap_top(heap h) {
-  ;
-  ;
-  ;
-  return NULL;
+  if(heap_empty(h)) return NULL;
+  return h->array[1];
 }
 
+//manque cette fonction
 void *heap_pop(heap h) {
-  ;
-  ;
-  ;
-  return NULL;
+  if(heap_empty(h)) return NULL;
+  void* premier = heap_top(h);
+  h->array[1] = h->array[h->n];
+  h->array[(h->n)--] = premier;
+  return premier;
 }
